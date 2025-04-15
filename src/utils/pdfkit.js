2b403bd84a80f1ec71f171aab300dbadd5edd271
sabah-hue <sabah.abdelbaset@gmail.com>
2025-04-15 06:19:32 +0200

    fix invoice

--- src/utils/pdfkit.js
+++ src/utils/pdfkit.js
@@ -292,5 +292,5 @@ function formatDate(date) {
   const day = date.getDate()
   const month = date.getMonth() + 1
   const year = date.getFullYear()
-  return ${year}/${month}/${day}
+  return `${year}/${month}/${day}`
 }
